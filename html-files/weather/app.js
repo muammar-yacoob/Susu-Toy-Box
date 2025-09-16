b7565e1d823eec83c8477db9a974deac58c6a36f
Yacoob <yacoob@spark-games.co.uk>
2025-09-16 10:34:42 +0100

    weather

--- html-files/weather/app.js
+++ html-files/weather/app.js
@@ -1,26 +1,29 @@
 async function getWeather() {
     const city = document.getElementById('city').value;
     const result = document.getElementById('result');
+    const button = document.querySelector('button');
 
-    result.innerHTML = '<span class="loading loading-spinner"></span> Getting weather...';
+    button.innerHTML = '<span class="loading loading-spinner"></span>';
+    button.disabled = true;
 
-    try {
-        const response = await fetch(`http://wttr.in/${city}?format=3`);
-        const weather = await response.text();
+    const response = await fetch(`http://wttr.in/${city}?format=3`);
+    const weather = await response.text();
 
-        result.innerHTML = `
-            <div class="alert alert-success">
-                <img src="https://img.icons8.com/fluency/24/sun.png"/>
-                <span>${weather}</span>
-            </div>
-        `;
-    } catch {
-        result.innerHTML = `
-            <div class="alert alert-error">
-                <img src="https://img.icons8.com/fluency/24/error.png"/>
-                <span>Could not get weather!</span>
-            </div>
-        `;
-    }
+    let icon = 'clear-day.svg';
+    if (weather.includes('rain')) icon = 'rainy.svg';
+    if (weather.includes('snow')) icon = 'snowy.svg';
+    if (weather.includes('cloud')) icon = 'cloudy-day.svg';
+    if (weather.includes('storm')) icon = 'thunder.svg';
+
+    result.innerHTML = `<img src="weather-icons/${icon}" width="80"/><div class="bg-gray-700 p-4 rounded-lg text-white mt-4">${weather}</div>`;
+
+    button.innerHTML = 'Get Weather!';
+    button.disabled = false;
+}
+
+async function fillCity() {
+    const response = await fetch('http://ip-api.com/json');
+    const data = await response.json();
+    document.getElementById('city').value = data.city;
 }
 
